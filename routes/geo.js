38f5fa5bad03b876cbf881b28afe976b8585c0ea
Xin <weijacky78@hotmail.com>
2023-05-05 13:04:43 -0400

    added gatineau hydrants json data fetch feature

--- routes/geo.js
+++ routes/geo.js
@@ -2,6 +2,7 @@
 const router = require('express').Router();
 const axios = require('axios');
 const cors = require('cors');
+const { map } = require('../app');
 
 router.use(cors());
 
@@ -18,6 +19,29 @@ router.get('/', async function (req, res, next) {
     // console.log(fetch.data);
     res.json({ lat: lat, lng: lng });
 });
+
+/* GET home page. */
+router.get('/hydrants', async function (req, res, next) {
+
+    let url = `https://www.gatineau.ca/upload/donneesouvertes/BORNE_FONTAINE.json`;
+    let fetch = await axios.get(url);
+    let mapData = fetch.data.features;
+    let outData = [];
+    let outCount = mapData.length > 20 ? 20 : mapData.length;
+
+    for (let i = 0; i < outCount; i++) {
+        let rnd = Math.floor(Math.random() * mapData.length);
+        let out = {
+            "lat": mapData[rnd].geometry.coordinates[1],
+            "lng": mapData[rnd].geometry.coordinates[0],
+            "setId": i,
+            "spec": mapData[rnd].properties.SPECIFIQUE
+        };
+        outData.push(out);
+    }
+    res.json(outData);
+});
+
 
 
 
